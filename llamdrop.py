67a77f329230a6a3a49b2f4f6934e8c3436f111f
DeVenLucaz <ypatole035@gmail.com>
2026-04-27 00:03:17 +0530

    Update llamdrop.py

--- llamdrop.py
+++ llamdrop.py
@@ -11,7 +11,7 @@ import curses
 import json
 import time
 
-VERSION = "0.7.0-dev"
+VERSION = "0.7.1-dev"
 
 # Ensure modules directory is on path
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
